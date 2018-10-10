ccd3055658b9525ca3b7293b410a7dbcb30358cb
unknown <dumas.clt@gmail.com>
2018-10-10 13:51:16 +0200

    addFav

--- src/addFav.js
+++ src/addFav.js
@@ -5,8 +5,27 @@ const addFav = {
 
     },
     setFav(e){
-        this.favList.push(e);
+        let test = -1;
+        test = this.favList.indexOf(e);
+        if(test === -1){
+            this.favList.push(e);
+
+        }
+
         console.log(this.favList);
+    },
+    addItem(e){
+        let newItem = new ListFav();
+        newItem.build(e.detail);
+        this.el.appendChild(newItem.el);
+        console.log(newItem);
+    },
+}
+
+class ListFav{
+
+    build(){
+        this.el = document.createElement('article');
     }
 }
 
